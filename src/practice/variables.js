020ad6036c711998f6274c0b92fd7b7765068f30
Oluseyi Anani <anasey2018@gmail.com>
2018-04-26 09:57:09 +0100

    Solution to variable.JS

---
+++ src/practice/variables.js
@@ -0,0 +1,52 @@
+/* 
+  Problem 1: Create a variable named `name` and set it equal to your first and last name 
+  Does this variable need to be a constant? It depends completely on you!
+  It should be a const variable IF you intend to never change your name.
+*/
+
+// code for `name` goes here
+const name = 'Oluseyi';
+/*
+  Problem 2: create a variable called `age`:
+  UNLESS you've descovered a very important secret in life, you should probably allow for `mutability` here.
+*/
+
+// code for `age` goes here
+let age = 29;
+/*
+  Problem create a variable called `location` and save it to where you're currently sitting. 
+  LIKE above, you may want to consider if this is something that could change.
+*/
+
+// code for `location` goes here
+let location = 'Lagos, Nigeria'
+/*
+  Problem create a variable called `faveBook` 
+  LIKE above, you may want to consider if this is some data about yourself that could change.
+*/
+
+// code for `faveBook` goes here
+const faveBook = ['Gone With the Wind', 'The Lord of the Rings', 'The Catcher in the Rye', 'Little Women']
+/*
+  Problem create a variable called `faveBand` 
+  LIKE above, you may want to consider if this is some data about yourself that could change.
+  MY favorite band will NEVER change. They have been my favorite band since I was 13 years old. (ask me sometime ;) ) 
+*/
+
+// code for `faveBand` goes here
+const faveBand = ['AC/DC', 'The Beatles'];
+/*
+  Problem create a variable called `faveFood` 
+  LIKE above, you may want to consider if this is some data about yourself that could change.
+*/
+
+// code for `faveFood` goes here
+const faveFood = ['Spagetti', 'Chinese rice'];
+module.exports = {
+  name,
+  age,
+  location,
+  faveBook,
+  faveBand,
+  faveFood
+};
